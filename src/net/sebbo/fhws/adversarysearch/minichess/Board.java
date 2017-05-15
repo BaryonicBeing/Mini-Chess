81ecf4564710530cd9b25f92ed8b28c70e007bd0
Sebastian Pekarek <mail@sebbo.net>
2017-05-15 13:51:18 +0200

    Board Tests 🙌🏼

--- src/net/sebbo/fhws/adversarysearch/minichess/Board.java
+++ src/net/sebbo/fhws/adversarysearch/minichess/Board.java
@@ -1,12 +1,13 @@
 package net.sebbo.fhws.adversarysearch.minichess;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.stream.Stream;
 
 /**
  * Created by Sebastian Pekarek on 15.05.17.
  */
 public class Board {
-    private char[][] squares = new char[5][6];
+    private char[][] squares = new char[6][5];
     private String parseError = null;
     private int moveNum = 1;
     private char onMove;
@@ -14,12 +15,12 @@ public class Board {
     public Board() {
         this.parseString(
             "1 W\n" +
-            "  kqbnr\n" +
-            "  ppppp\n" +
-            "  .....\n" +
-            "  .....\n" +
-            "  PPPPP\n" +
-            "  RNBQK"
+            "kqbnr\n" +
+            "ppppp\n" +
+            ".....\n" +
+            ".....\n" +
+            "PPPPP\n" +
+            "RNBQK"
         );
     }
 
@@ -52,20 +53,27 @@ public class Board {
             this.onMove = 'B';
         }
 
-        for(; i <= importString.length(); i += 1) {
+        for(i = importString.indexOf("\n") + 1; i <= importString.length(); i += 1) {
             thisChar = importString.charAt(i);
 
-            if(thisChar == '\n' && column == (squares[0].length - 1) && row <= squares.length) {
+            if(thisChar == '\n' && column == squares[0].length && row < squares.length) {
                 row += 1;
                 column = 0;
             }
-            else if(thisChar == '\n' && column == (squares[0].length - 1)) {
+            else if(thisChar == '\n' && column == squares[0].length) {
                 throw new Exception("Error while parsing board: Got new line, but board is already complete…");
             }
             else if(thisChar == '\n') {
-                throw new Exception("Error while parsing board: Got new line, but current column is not complete…");
+                throw new Exception(
+                    "Error while parsing board: Got new line, but current row (" + row + ") is not complete. "+
+                    "Has only column " + column + " of " + squares[0].length + "…"
+                );
             }
             else if(thisChar == '.' || thisChar == ' ') {
+                this.squares[row][column] = '.';
+                column += 1;
+            }
+            else {
                 this.squares[row][column] = thisChar;
                 column += 1;
             }
@@ -75,7 +83,13 @@ public class Board {
     public String toString() {
         StringBuilder output = new StringBuilder();
 
+        output.append(moveNum);
+        output.append(' ');
+        output.append(onMove);
+
         for (char[] columns : this.squares) {
+            output.append("\n");
+
             for (char column : columns) {
                 output.append(column);
             }
@@ -91,17 +105,17 @@ public class Board {
     public static void main(String[] args){
 
         String toCompare = "1 W\n" +
-                "  kqbnr\n" +
-                "  ppppp\n" +
-                "  .....\n" +
-                "  .....\n" +
-                "  PPPPP\n" +
-                "  RNBQK";
+                "kqbnr\n" +
+                "ppppp\n" +
+                ".....\n" +
+                ".....\n" +
+                "PPPPP\n" +
+                "RNBQK";
 
-        System.out.println("START TESTING!");
+        System.out.println("# Board Unit Tests");
 
         Board standardBoard = new Board();
-        System.out.println("Starting test for standard constructor!");
+        System.out.println("\n## Starting test for standard constructor!");
         if(standardBoard != null){
             System.out.println("not null test successful!");
         }else{
@@ -111,17 +125,18 @@ public class Board {
         if(standardBoard.toString().equals(toCompare)){
             System.out.println("toString test successful!");
         }else{
-            System.out.println("toString test failed!");
+            System.out.println("toString test failed!\n" + standardBoard.toString());
             System.out.println(standardBoard.parseError);
         }
 
-        System.out.println("Start test for constructor with String as parameter!");
-        Board stringBoard = new Board("  kqbnr\n" +
-                "  ppppp\n" +
-                "  .....\n" +
-                "  .....\n" +
-                "  PPPPP\n" +
-                "  RNBQK");
+        System.out.println("\n## Start test for constructor with String as parameter!");
+        Board stringBoard = new Board("1 W\n" +
+                "kqbnr\n" +
+                "ppppp\n" +
+                ".....\n" +
+                ".....\n" +
+                "PPPPP\n" +
+                "RNBQK");
         if(stringBoard != null){
             System.out.println("not null test successful!");
         }else{
