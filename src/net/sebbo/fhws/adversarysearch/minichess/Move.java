57ebe32a75765757c89233a2d014df55dc863c71
max <klemm.maximilian@web.de>
2017-05-15 11:52:15 +0200

    Refactored class Move

--- src/net/sebbo/fhws/adversarysearch/minichess/Move.java
+++ src/net/sebbo/fhws/adversarysearch/minichess/Move.java
@@ -4,18 +4,18 @@ package net.sebbo.fhws.adversarysearch.minichess;
  * Created by max on 15.05.17.
  */
 public class Move {
-    int[] from = new int[2];
-    int[] to = new int[2];
+    //int[] from = new int[2];
+    //int[] to = new int[2];
+    Square from;
+    Square to;
 
     public Move(String fromto){
         char[] positions = fromto.toCharArray();
-        this.from[0] = positions[0] - 97;
-        this.from[1] = positions[1] - 48;
-        this.to[0] = positions[2] - 97;
-        this.to[1] = positions[3] - 48;
+        this.from = new Square(positions[0]+ "" + positions[1]);
+        this.to = new Square(positions[2] + "" + positions[3]);
     }
 
     public String toString(){
-        return "Moved from " + from[0] + "" + from[1] + " to " + to[0] + "" + to[1] + ".";
+        return "Moved from " + from.toString() + " to " + to.toString() + ".";
     }
 }
