2eda13f05688854a667a677a8209666c898e244c
Sebastian Pekarek <mail@sebbo.net>
2017-05-15 15:50:14 +0200

    Square: isOccupied()

--- src/net/sebbo/fhws/adversarysearch/minichess/Square.java
+++ src/net/sebbo/fhws/adversarysearch/minichess/Square.java
@@ -29,6 +29,9 @@ public class Square {
     public char getOccupiedBy() {
         return this.occupiedBy;
     }
+    public boolean isOccupied() {
+        return this.occupiedBy != '.';
+    }
 
     public char getFigureType() {
         return Character.toLowerCase(this.occupiedBy);
